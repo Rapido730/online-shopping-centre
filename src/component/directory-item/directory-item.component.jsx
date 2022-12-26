b19dccca6f2197e124546f45779bb981603de0bb
Rapido730 <sharmamit510@gmail.com>
2022-12-26 12:23:00 +0530

    final touches added links in directory items

--- src/component/directory-item/directory-item.component.jsx
+++ src/component/directory-item/directory-item.component.jsx
@@ -1,7 +1,11 @@
 import "./directory-item.style.scss";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(`/shop/${title}`)
   return (
     <div className="directory-item-container">
       <div
@@ -10,7 +14,7 @@ const DirectoryItem = ({ category }) => {
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="directory-item-body-container">
+      <div className="directory-item-body-container" onClick={onNavigateHandler}>
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -1,7 +1,35 @@
 import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styles.scss"
 
-const Directory = ({CategoriesList}) => {
+const CategoriesList = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+const Directory = () => {
   
   return (
     <div className="directory-container">
@@ -13,3 +41,4 @@ const Directory = ({CategoriesList}) => {
 };
 
 export default Directory;
+  
